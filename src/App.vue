4e8c89e0e6c3dc17728efaef7ae95eed08b1e407
Alec Carpenter <alec.carpenter@homeadvisor.com>
2019-12-27 17:27:25 -0500

    Adds the basics

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WidgetBoard
+      :wrapperClass="$style.wrapper"
+      :widgetClass="$style.widget"
+      :config="getConfig()"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {WidgetBoard} from 'dashboard-lib';
 
 export default {
   name: 'app',
+  methods: {
+    getConfig() {
+      return {
+        widgets: [
+          {
+            name: 'countdown',
+            props: {
+              since: '2019-12-27',
+              until: '2021-06-13',
+              title: '24 Hours of Le Mans'
+            }
+          },
+          {
+            name: 'vehicle-status',
+            props: {
+              vin: 'WVWWA7AU2KW223707'
+            }
+          }
+        ]
+      }
+    }
+  },
   components: {
-    HelloWorld
+    WidgetBoard
   }
 }
 </script>
@@ -21,8 +46,30 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+}
+</style>
+
+<style module>
+.wrapper {
+  display: flex;
+  padding: 1em;
+  flex-direction: column;
+}
+
+.widget {
+  margin: 0 0 1em;
+}
+
+@media screen and (min-width: 600px) {
+  .wrapper {
+    flex-direction: row;
+  }
+
+  .widget {
+    width: 25%;
+    margin: 0 1em 1em 0;
+  }
 }
 </style>
