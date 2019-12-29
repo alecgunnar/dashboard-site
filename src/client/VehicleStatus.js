cd78984b11330e58c458d0a1c831094c4a25a22b
Alec Carpenter <alec.carpenter@homeadvisor.com>
2019-12-29 14:24:34 -0500

    Updates vehicle status url

--- src/client/VehicleStatus.js
+++ src/client/VehicleStatus.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default {
   async getStatus (vin) {
-    return (await axios.get(`${process.env.VUE_APP_MCP_URL}dashboard/vehicle/${vin}`)).data
+    return (await axios.get(`${process.env.VUE_APP_MCP_URL}dashboard/vehicles/${vin}`)).data
   }
 }
